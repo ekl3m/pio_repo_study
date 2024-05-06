8541c4989218c55f7c4edfcd6636001dfcbeec1f
ekl3m <eryk.klem03@gmail.com>
2024-05-06 14:42:48 +0200

    Dzial I, 5. : Commit techniczny, próba naprawy nazewnictwa

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -3,9 +3,9 @@ package edu.kis.vh.nursery.factory;
 import edu.kis.vh.nursery.DefaultCountingOutRhymer;
 import edu.kis.vh.nursery.FIFORhymer;
 import edu.kis.vh.nursery.HanoiRhymer;
-import edu.kis.vh.nursery.factory.RhymersFactory;
+import edu.kis.vh.nursery.factory.Rhymersfactory;
 
-public class DefaultRhymersFactory implements RhymersFactory {
+public class DefaultRhymersFactory implements Rhymersfactory {
 
     @Override
     public DefaultCountingOutRhymer getStandardRhymer() {
